1e6d355538f6d058f4b115de8daa63fac3eaa832
Azhad Ghufran <azhad56@gmail.com>
2020-07-10 00:50:09 +0530

    added slideshow

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,5 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 function ins1()
 {
 	var text = document.getElementById("p1").innerHTML="sjhjksdjksjkjk";
@@ -5,4 +7,30 @@ function ins1()
 function contact()
 {
 	document.getElementById("slider").style.display="none";
+}
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
