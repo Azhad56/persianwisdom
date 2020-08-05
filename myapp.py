3a9677c057b13a9a8d0e430793e9d762e9dabc73
Azhad Ghufran <azhad56@gmail.com>
2020-08-05 17:50:45 +0530

    update

--- myapp.py
+++ myapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-@app.route("/author",methods = ['Get','Post'])
+@app.route("/author/")
 def author():
     return render_template("author_guidelines.html")
 if __name__ =='__main__':
